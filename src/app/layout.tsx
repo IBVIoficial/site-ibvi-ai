ee53c7ac76fdee9036197df52fdb5728eaa272aa
Ronaldo Lima <username@users.noreply.github.com>
2025-04-01 17:31:48 -0300

    IBVI brand

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import '@/styles/tailwind.css'
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - Studio',
-    default: 'Studio - Award winning developer studio based in Denmark',
+    template: '%s - IBVI',
+    default: 'IBVI - A revolução do mercado imobiliário brasileiro',
   },
 }
 
